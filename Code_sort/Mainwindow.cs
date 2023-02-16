79854ad1821928ea6726386e8465559b1bb453ea
Lisdery <Lisdery@126.com>
2023-02-16 17:15:32 +0800

    add initpanel

--- Code_sort/Mainwindow.cs
+++ Code_sort/Mainwindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,30 @@ namespace Code_sort
 {
     public partial class Mainwindow : Form
     {
+        readonly Dictionary<string, Panel> scenes = new();
         public Mainwindow()
         {
             InitializeComponent();
+
+            this.Size = new System.Drawing.Size(600, 1000);
+
+            Panel start_panel = new Panel();
+            scenes.Add("Start_Panel",start_panel);
+
+            start_panel.Dock = DockStyle.Fill;
+            start_panel.Parent= this;
+
+            InitPanel("Start_Panel");
+        }
+
+        public void InitPanel (string name)
+        {
+            switch (name)
+            {
+                case "Start_Panel":
+
+                    break;
+            }
         }
     }
 }
