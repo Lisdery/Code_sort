f40e0e03f6788e694dbbaec0f98a771cfd401d9d
Lisdery L <Lisdery@126.com>
2023-02-19 17:26:04 +0800

    edo

---
+++ Code_sort/GlobalInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_sort
+{
+    internal class GlobalInfo
+    {
+        public static int Window_width = 600;
+        public static int Window_height = 400;
+        public static int DM_Button_width = 300;
+        public static int DM_Button_height = 50;
+    }
+}
