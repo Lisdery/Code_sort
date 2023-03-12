519bb0eb4044188619fa9315e430108fd4894119
Lisdery L <Lisdery@126.com>
2023-03-12 13:53:03 +0800

    nothing

--- Code_sort/Mainwindow.cs
+++ Code_sort/Mainwindow.cs
@@ -19,57 +19,66 @@ namespace Code_sort
         public Mainwindow()
         {
             InitializeComponent();
-            this.Size = new Size()
+            InitForm();
+
+            Size = new Size()
             {
                 Height = info.Window_height,
                 Width = info.Window_width
             };
-
-            Panel start_panel = new Panel()
-            {
-                Parent = this,
-                Size = this.Size,
-                Padding = new Padding(0, 0, 0, 0),
-                Location = new Point(0,0),
-            };
-
+            Panel start_panel = new Panel();
             scenes.Add("Start_Panel", start_panel);
 
+            scenes["Start_Panel"].Parent = this;
+            
             Init_Panel("Start_Panel");
+
         }
 
+        private void InitForm()
+        {
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Padding = new Padding(0, 0, 0, 0);
+            Margin = new Padding(0, 0, 0, 0);
+        }
         public void Init_Panel(string name)
         {
             switch (name)
             {
                 case "Start_Panel":
                     Panel panel = scenes[name];
-
-                    MessageBox.Show($"{panel.Location.X}");
-
-                    MessageBox.Show($"{panel.Location.Y}");
-
+                    panel.Size = Size;
+                    panel.Location = new Point(0, 0);
+                    panel.Padding = new Padding(0, 0, 0, 0);
+                    panel.Margin = new Padding(0, 0, 0, 0);
+                    panel.Dock = DockStyle.Fill;
+                    panel.AutoSize = false;
+                    panel.BackColor = Color.White;
                     Button Determine_Button = new()
                     {
                         Name = "DM_button",
-                        Parent = panel,
                         Text = "Search!",
-                        Visible = true,
-                        Padding = new Padding(0,0,0,0),
-                        Location = new Point()
-                        {
-                            X = (info.Window_height - info.DM_Button_height)/2,
-                            Y = (info.Window_width - info.DM_Button_width)/2
-                        },
                         Size = new Size()
                         {
                             Height = info.DM_Button_height,
                             Width = info.DM_Button_width
-                        }
+                        },
+                        Location = new Point()
+                        {
+                            X = (info.Window_width - info.DM_Button_width) / 2,
+                            Y = (info.Window_height - info.DM_Button_height) / 2+100
+                        },
+                        Parent = panel,
+                        Visible = true,
+                        Padding = new Padding(0, 0, 0, 0),
+                        Margin = new Padding(0, 0, 0, 0),
+                        AutoSize = false,
+                        TextAlign = ContentAlignment.MiddleCenter,
                     };
-                    MessageBox.Show($"{panel.Size.Height}");
+                    MessageBox.Show($"doka {Height}");
 
-                    MessageBox.Show($"{panel.Size.Width}");
+                    MessageBox.Show($"doka {Width}");
 
                     MessageBox.Show($"{Determine_Button.Location.X}");
 
